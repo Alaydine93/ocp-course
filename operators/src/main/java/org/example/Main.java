ba8aa1e36eb7b65116b75c179cac36cac9a23be8
alae-eddine <Ronneymar10!>
2023-06-17 11:42:55 +0200

    ternary operation examples

--- operators/src/main/java/org/example/Main.java
+++ operators/src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer intNumber = 3;
+        double number = 1;
+        openZoo(intNumber);
+        openZoo(null);
+
+        int owl = 1;
+        int food1 = owl < 4 ? owl > 2 ? 3 : 4 : 5;
+        System.out.println("ternary result : " + food1);
+
+        var stripes = 11;
+
+        var animal = stripes < 9 ? 3 : "Horse";
+
+        System.out.println(animal);
+    }
+
+    public static void openZoo(Number time) {
+        if (time instanceof Integer)
+            System.out.println((Integer) time + " O'clock");
+        else
+            System.out.println(time);
     }
 }
