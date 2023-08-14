b4b01e7f436dd483ffbc0ef2465ac49e4d08e522
alae-eddine <Ronneymar10!>
2023-08-14 11:29:11 +0200

    reviewing advanced stream concepts

--- streams/src/main/java/org/example/AdvancedConcepts.java
+++ streams/src/main/java/org/example/AdvancedConcepts.java
@@ -1,31 +1,31 @@
 package org.example;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.Spliterator;
 import java.util.TreeMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AdvancedConcepts {
 
-    public static void main(String... args){
-        divideBag();
+    public static void main(String... args) {
+        seperations();
     }
 
-    private static void threeDigit(Optional<Integer> optional){
+    private static void threeDigit(Optional<Integer> optional) {
         optional.map(n -> "" + n)
                 .filter(s -> s.length() == 3)
                 .ifPresent(System.out::println);
     }
 
-    private static void divideBag(){
+    private static void divideBag() {
         var stream = List.of("bird-", "bunny-", "cat-", "dog-", "fish-", "lamb-", "mouse-");
         Spliterator<String> originalBagOfFood = stream.spliterator();
         Spliterator<String> emmasBag = originalBagOfFood.trySplit();
-
         emmasBag.forEachRemaining(System.out::print);
         System.out.println();
         Spliterator<String> jillsBag = originalBagOfFood.trySplit();
@@ -46,17 +46,57 @@ public class AdvancedConcepts {
         System.out.println(result);
     }
 
-    private static void streamToMap(){
+    private static void streamToSet() {
         var stream = Stream.of("lions", "tigers", "bears");
         TreeMap<Integer, List<String>> map = stream.collect(Collectors.groupingBy(
                 String::length,
                 TreeMap::new,
                 Collectors.toList()));
+        System.out.println(map);
+    }
+
+    private static void streamToMap() {
+        var stream = Stream.of("lions", "tigers", "bears");
+        Map<String, Integer> map = stream.collect(Collectors.toMap(Function.identity(), String::length));
+        System.out.println(map);
+    }
+
+    private static void reverseStreamToMap() {
+        var stream = Stream.of("lions", "tigers", "bears");
+        TreeMap<Integer, String> map = stream.collect(Collectors.toMap(
+                String::length,
+                Function.identity(),
+                (s1, s2) -> s1 + ", " + s2,
+                TreeMap::new));
+        System.out.println(map);
     }
 
     private static void partitioningStream() {
         var stream = Stream.of("lions", "tigers", "bears");
         Map<Boolean, List<String>> map = stream.collect(Collectors.partitioningBy(s -> s.length() <= 5, Collectors.toList()));
+    }
 
+    private static void groupingByLength() {
+        var stream = Stream.of("lions", "tigers", "bears");
+        Map<Integer, Long> map = stream.collect(Collectors.groupingBy(String::length, Collectors.counting()));
+        System.out.println(map);
+    }
+
+    private static void mappingBy() {
+        var stream = Stream.of("lions", "tigers", "bears");
+        Map<Integer, Optional<Character>> map = stream.collect(
+                Collectors.groupingBy(
+                        String::length,
+                        Collectors.mapping(s -> s.charAt(0), Collectors.minBy((a,b)->a -b))));
+        System.out.println(map);
+    }
+
+    private static void seperations() {
+        var list = List.of("x", "y", "y");
+        Separations result = list.stream().collect(
+                Collectors.teeing(
+                        Collectors.joining(" "),
+                        Collectors.joining(","), (s,c) -> new Separations(s,c)));
+        System.out.println(result);
     }
 }
---
+++ streams/src/main/java/org/example/Separations.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record Separations(String spaceSeparated, String commaSeparated) {
+}
